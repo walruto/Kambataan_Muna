9897f6384ed3e7b8042e4adf6f3d1fbb2385bc82
walruto <walidfeysell@gmail.com>
2025-07-01 15:31:36 -0700

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -122,7 +122,7 @@ const Hero = () => {
           </div>
           <div className="relative group overflow-hidden rounded-2xl">
             <img 
-              src="https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=400"
+              src="https://scontent-sjc3-1.cdninstagram.com/v/t51.2885-19/470935536_500937152382602_8780750495611077646_n.jpg?stp=cp0_dst-jpg_s110x80_tt6&_nc_cat=107&ccb=1-7&_nc_sid=bf7eb4&_nc_ohc=EjEW4m7ULpEQ7kNvwHNhOF0&_nc_oc=AdlKfi7aDUlNiag-3IUXokso6hZVipjyPkEapQKHUSFeQIQS6YdUf94NT99Q-JtjAsQ&_nc_zt=24&_nc_ht=scontent-sjc3-1.cdninstagram.com&oh=00_AfM4y9Y2mJMQ_EwvsaIAY-I9Vfm5O6uL1Mu1Rq_6kgaL_A&oe=686A22490"
               alt="Youth Programs"
               className="w-full h-32 object-cover group-hover:scale-110 transition-transform duration-500"
             />
